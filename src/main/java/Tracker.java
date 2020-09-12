e717ddfc2975f1bb4a13abb164e091bd92a23ab4
Sylvester Chin <sylvester.csw@gmail.com>
2020-09-12 15:54:53 +0800

    fix: get registry instead of create

--- src/main/java/Tracker.java
+++ src/main/java/Tracker.java
@@ -73,7 +73,7 @@ public class Tracker implements TrackerRMI {
         try {
             Tracker t = new Tracker(port, n, k);
             stub = (TrackerRMI) UnicastRemoteObject.exportObject(t, 0);
-            registry = LocateRegistry.createRegistry(port);
+            registry = LocateRegistry.getRegistry(port);
             registry.bind("TrackerRMI", stub);
 
             System.out.println("Tracker Ready");
